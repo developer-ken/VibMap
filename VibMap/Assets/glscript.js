418902c45c151d60482c3a02b953390bda26f8a9
dengbw01 <dengbw01@outlook.com>
2022-01-15 21:22:20 +0800

    定位机制实现，基本功能完成

---
+++ VibMap/Assets/glscript.js
@@ -0,0 +1,16 @@
+﻿var lineLayer = new mapvgl.LineRainbowLayer({
+	style: 'road', // road, arrow, normal
+	width: 15,
+	color: [{ $COLORS }]
+});
+view.addLayer(lineLayer);
+
+var data = [{
+	geometry: {
+		type: 'LineString',
+		coordinates: [
+			{ $CORDS }
+		]
+	}
+}];
+lineLayer.setData(data);
--- VibMap/MainActivity.cs
+++ VibMap/MainActivity.cs
@@ -12,12 +12,26 @@ using System.IO;
 using Android.Locations;
 using Xamarin.Essentials;
 using System.Collections.Generic;
+using AndroidX.Core.App;
+using Android;
+using Android.Util;
 
 namespace VibMap
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, ILocationListener
     {
+        LocationManager lm;
+
+        double LastAcc = -1;
+        double SumDAcc = 0;
+        bool runFlag = true;
+        Android.Locations.Location LastLocation;
+
+        Dictionary<double[], double> Points =
+            new System.Collections.Generic.Dictionary<double[], double>();
+        double minvib = double.MaxValue, maxvib = double.MinValue;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -35,13 +49,14 @@ namespace VibMap
             web.SetWebViewClient(new MyWebViewClient());
             StreamReader input = new StreamReader(Assets.Open("webmap.html"));
             web.LoadDataWithBaseURL("https://mapv.baidu.com/gl/examples/editor.html#line-color.html", input.ReadToEnd(), "text/html", "utf-8", "https://mapv.baidu.com/gl/examples/editor.html");
-            LocationManager lm = (LocationManager)GetSystemService(LocationService);
+            input.Close();
+            lm = (LocationManager)GetSystemService(LocationService);
+
+            ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.AccessFineLocation, Manifest.Permission.AccessMockLocation }, 1);
+
             Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
             Accelerometer.Start(SensorSpeed.UI);
         }
-
-        double LastAcc = -1;
-        double SumDAcc = 0;
 
         private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
         {
@@ -74,34 +89,46 @@ namespace VibMap
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
             WebView web = FindViewById<WebView>(Resource.Id.webview);
+            string colorcode = "", locationcode = "";
             var ratio = 0xFF / (maxvib - minvib);
             foreach (var p in Points)
             {
+                if (colorcode.Length > 0) colorcode += ",";
+                if (locationcode.Length > 0) locationcode += ",";
                 var colorR = (p.Value - minvib) * ratio;
-                string R = colorR.ToString("X");
-                web.EvaluateJavascript("AddData(" + p.Key.Latitude + "," + p.Key.Longitude + ",\"#" + R + "0000\");", null);
+                string R = ((int)colorR).ToString("X2");
+                colorcode += "\"#" + R + "0000\"";
+                locationcode += "[" + p.Key[0] + "," + p.Key[1] + "]";
+                //web.EvaluateJavascript("linevalue.push(\"#" + R + "0000\");pointArr.push(new BMap.Point( " + p.Key[0] + "," + p.Key[1] + "));", null);
             }
-            web.EvaluateJavascript("Finish();", null);
+            StreamReader inp = new StreamReader(Assets.Open("glscript.js"));
+            string script = inp.ReadToEnd()
+                .Replace("{ $COLORS }", colorcode)
+                .Replace("{ $CORDS }", locationcode);
+            inp.Close();
+            web.EvaluateJavascript(script, null);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            if (requestCode == 1 && grantResults[0] == Android.Content.PM.Permission.Granted)
+            {
+                Log.Info("permission", "LOCATION granted.");
+                lm.RequestLocationUpdates(LocationManager.GpsProvider, 500, 1, this);
+            }
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-
-        bool runFlag = false;
-        Android.Locations.Location LastLocation;
-
-        Dictionary<Android.Locations.Location, double> Points =
-            new System.Collections.Generic.Dictionary<Android.Locations.Location, double>();
-        double minvib = double.MaxValue, maxvib = double.MinValue;
 
         public void OnLocationChanged(Android.Locations.Location location)
         {
             WebView web = FindViewById<WebView>(Resource.Id.webview);
-            web.EvaluateJavascript("Center(" + location.Latitude + "," + location.Longitude + ");", null);
+            var locationbd = LocationConverter.WGS84_to_BD09(location.Longitude, location.Latitude);
+            Log.Info("Location", "GPS=>" + location.Latitude + "," + location.Longitude + ".");
+            Log.Info("js-bridge", "Center=>" + locationbd[1] + "," + locationbd[0] + ".");
+
+            web.EvaluateJavascript("map.setCenter(new BMap.Point(" + locationbd[0] + "," + locationbd[1] + "));", null);
+            //lm.RequestLocationUpdates(LocationManager.FusedProvider, 1000, 1, this);
             if (runFlag)
             {
                 var dacc = SumDAcc;
@@ -114,9 +141,11 @@ namespace VibMap
                 var Distance = DistanceBetween(LastLocation, location);
                 LastLocation = location;
                 dacc = dacc / Distance;
-                Points.Add(location, dacc);
-                if (dacc > minvib) minvib = dacc;
-                if (dacc < maxvib) maxvib = dacc;
+                if (dacc.Equals(double.NaN)) return;
+                Points.Add(locationbd, dacc);
+                if (dacc < minvib) minvib = dacc;
+                if (dacc > maxvib) maxvib = dacc;
+                Log.Info("js-bridge", "Vibraition Level=" + dacc);
             }
         }
 
